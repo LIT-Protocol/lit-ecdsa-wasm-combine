956ce61d3e6c97260d78ae197c5e2f600d172b39
Josh Long <josh@litprotocol.com>
2023-07-28 17:43:13 -0400

    add better type conversion error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,9 @@
 extern crate console_error_panic_hook;
 extern crate wasm_bindgen;
 extern crate web_sys;
-use elliptic_curve::AffinePoint;
-use elliptic_curve::ProjectivePoint;
-use elliptic_curve::group::prime::PrimeCurveAffine;
+
 use elliptic_curve::sec1::ToEncodedPoint;
 use js_sys::Array;
-use js_sys::Uint8Array;
 use k256::Secp256k1;
 use std::panic;
 use wasm_bindgen::prelude::*;
@@ -53,22 +50,38 @@ pub fn combine_signature(in_shares: Array, key_type: u8) -> String {
 #[doc = "Entry point for compute hd derived public keys"]
 pub fn compute_public_key(id: String, public_keys: Array) -> String {
     panic::set_hook(Box::new(console_error_panic_hook::hook));
-    /* 
+    /*
         compressed -> 33
         uncompressed -> 65
     */
     let mut hd_pub_keys = Vec::with_capacity(public_keys.length() as usize);
     for pubkey in public_keys.iter() {
-        let hex_pub_key = hex::decode(pubkey.as_string().unwrap()).unwrap();
+        let pubkey = pubkey.as_string();
+        if pubkey.is_none() {
+            panic!("Could not covnert pubkey to string, aborting");
+        }
+        let pubkey = pubkey.unwrap();
+        let hex_pub_key = hex::decode(pubkey);
+        if hex_pub_key.is_err() {
+            panic!(
+                "Error while converting pubkey to hex encoding {}",
+                hex_pub_key.err().unwrap()
+            );
+        }
+        let hex_pub_key = hex_pub_key.unwrap();
         let a_p = convert_to_point(hex_pub_key.as_slice());
         hd_pub_keys.push(a_p);
     }
 
     let id = id.as_bytes();
 
-    let deriver =
-        combiners::hd_ecdsa::HdKeyDeriver::<Secp256k1>::new(id, combiners::hd_ecdsa::CXT).unwrap();
+    let deriver = combiners::hd_ecdsa::HdKeyDeriver::<Secp256k1>::new(id, combiners::hd_ecdsa::CXT);
 
+    if deriver.is_err() {
+        panic!("Could not derive publick key {}", deriver.err().unwrap())
+    }
+
+    let deriver = deriver.unwrap();
     let pubkey = deriver.compute_public_key(&hd_pub_keys.as_slice());
     let pubkey = hex::encode(pubkey.to_encoded_point(true).as_bytes());
 
@@ -77,6 +90,7 @@ pub fn compute_public_key(id: String, public_keys: Array) -> String {
 
 fn convert_to_point(input: &[u8]) -> k256::ProjectivePoint {
     use k256::elliptic_curve::sec1::FromEncodedPoint;
-    
-    k256::ProjectivePoint::from_encoded_point(&k256::EncodedPoint::from_bytes(input).unwrap()).unwrap()
+
+    k256::ProjectivePoint::from_encoded_point(&k256::EncodedPoint::from_bytes(input).unwrap())
+        .unwrap()
 }
