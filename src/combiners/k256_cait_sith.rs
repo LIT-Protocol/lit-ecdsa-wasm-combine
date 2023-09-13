60d762d54974ab6edccab7bb5d87c4e30e092d72
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2023-09-12 17:23:14 -0700

    remove recovery id inversion.  use // @ts-nocheck so we can copy paste directly into the JS SDK without typescript errors

--- src/combiners/k256_cait_sith.rs
+++ src/combiners/k256_cait_sith.rs
@@ -1,7 +1,10 @@
-use crate::{models::{SignatureRecidHex, SignedDatak256}, errors::CombinationError};
+use crate::{
+    errors::CombinationError,
+    models::{SignatureRecidHex, SignedDatak256},
+};
 
 use super::cs_curve::combine_signature_shares;
-use elliptic_curve::{group::GroupEncoding, bigint:: Encoding };
+use elliptic_curve::{bigint::Encoding, group::GroupEncoding};
 use k256::{AffinePoint, Scalar, Secp256k1};
 
 #[derive(Clone, PartialEq, Debug)]
@@ -17,10 +20,8 @@ pub struct SignatureRecid {
     pub recid: u8,
 }
 
-
 #[doc = "Entry point for recombining signatures."]
 pub fn combine_signature(shares: Vec<String>) -> String {
-    
     let sig = combine_signature_internal(shares);
     if sig.is_err() {
         return sig.unwrap_err().to_string();
@@ -34,7 +35,6 @@ pub fn combine_signature(shares: Vec<String>) -> String {
     };
 
     serde_json::to_string(&sig_hex).unwrap()
-
 }
 
 #[doc = "Entry point for recombining signatures."]
@@ -56,7 +56,6 @@ fn combine_signature_internal(shares: Vec<String>) -> Result<SignatureRecid, Com
         .iter()
         .map(|x| x.signature_share)
         .collect::<Vec<Scalar>>();
-    
 
     let sig = do_combine_signature(public_key, presignature_big_r, msg_hash, shares);
 
@@ -83,13 +82,13 @@ pub fn do_combine_signature(
     // calc the recovery id
     use elliptic_curve::point::AffineCoordinates;
     use elliptic_curve::Curve;
-    let mut recid  =  if presignature_big_r.y_is_odd().into() { 1 } else { 0 };    
-    let s_bi = num_bigint::BigUint::from_bytes_be( &s.to_bytes());
-    let order = num_bigint::BigUint::from_bytes_be( &k256::Secp256k1::ORDER.to_be_bytes());
-
-    if s_bi > order - &s_bi {
-        recid ^= 1;
-    }
+    let mut recid = if presignature_big_r.y_is_odd().into() {
+        1
+    } else {
+        0
+    };
+    let s_bi = num_bigint::BigUint::from_bytes_be(&s.to_bytes());
+    let order = num_bigint::BigUint::from_bytes_be(&k256::Secp256k1::ORDER.to_be_bytes());
 
     SignatureRecid { r, s, recid }
 }
